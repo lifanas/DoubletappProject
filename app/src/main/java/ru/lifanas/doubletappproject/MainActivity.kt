29565c2aae5624619fe75187474a0db5854c0e79
lifanas <fakeittellyoumakeit2024@gmail.com>
2025-02-15 18:04:52 +0300

    add binding

--- app/src/main/java/ru/lifanas/doubletappproject/MainActivity.kt
+++ app/src/main/java/ru/lifanas/doubletappproject/MainActivity.kt
@@ -1,12 +1,22 @@
 package ru.lifanas.doubletappproject
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import ru.lifanas.doubletappproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
     }
+
+
 }
--- app/src/main/java/ru/lifanas/doubletappproject/SquareActivity.kt
+++ app/src/main/java/ru/lifanas/doubletappproject/SquareActivity.kt
@@ -3,18 +3,16 @@ package ru.lifanas.doubletappproject
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ru.lifanas.doubletappproject.databinding.ActivitySquareBinding
 
 class SquareActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySquareBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_square)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivitySquareBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
