f47fd97f90ddb0e62c54748090cbcb99b56c17dc
lifanas <fakeittellyoumakeit2024@gmail.com>
2025-02-15 18:27:54 +0300

    add logic for MainActivity

--- app/src/main/java/ru/lifanas/doubletappproject/MainActivity.kt
+++ app/src/main/java/ru/lifanas/doubletappproject/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import ru.lifanas.doubletappproject.databinding.ActivityMainBinding
+import ru.lifanas.doubletappproject.saverLog.SaverLog
 
 class MainActivity : AppCompatActivity() {
 
+    private var count = 0
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +16,47 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        incCount(savedInstanceState)
+        SaverLog.log(this, "$ACTIVITY_TAG: onCreate")
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COUNT, count)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        SaverLog.log(this, "$ACTIVITY_TAG: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        SaverLog.log(this, "$ACTIVITY_TAG: onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        SaverLog.log(this, "$ACTIVITY_TAG: onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SaverLog.log(this, "$ACTIVITY_TAG: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        SaverLog.log(this, "$ACTIVITY_TAG: onDestroy")
+    }
+
+    private fun incCount(savedInstanceState: Bundle?) {
+        count = savedInstanceState?.getInt(COUNT)?.inc() ?: 0
+        binding.displayedNumber.text = count.toString()
+    }
+
+    private companion object {
+        const val COUNT = "count"
+        const val ACTIVITY_TAG = "MainActivity"
+    }
 }
---
+++ app/src/main/java/ru/lifanas/doubletappproject/saverLog/SaverLog.kt
@@ -0,0 +1,7 @@
+package ru.lifanas.doubletappproject.saverLog
+
+import android.content.Context
+
+object SaverLog {
+    fun log(context: Context, message: String) {}
+}
